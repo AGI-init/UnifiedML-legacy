85c24d7d77354776cc18c47d5a7ebd97a82f4014
samlerman <agi.init@gmail.com>
2022-09-05 17:40:20 -0400

    Beta - __init__ file for import as package

---
+++ Hyperparams/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) AGI.__init__. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# MIT_LICENSE file in the root directory of this source tree.
+"""
+This file makes it possible to import UnifiedML as a package.
+
+For an example, see: github.com/agi-init/XRD.
+"""
