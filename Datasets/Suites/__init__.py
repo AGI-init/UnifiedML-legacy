60c5cc33dfb2378518c68e068bf6c01f812dfcf1
sam <slerman@u.rochester.edu>
2023-12-30 15:21:06 -0500

    Logos

--- Datasets/Suites/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) AGI.__init__. All Rights Reserved.
-#
-# This source code is licensed under the MIT license found in the
-# MIT_LICENSE file in the root directory of this source tree.
-from .Atari import Atari
-from .DMC import DMC
-from .Classify import Classify
-from .SuperMario import SuperMario
